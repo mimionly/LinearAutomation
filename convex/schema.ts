1a7a7b3600dbf7e92bee4b5c496e37e71eb75293
risha-bit <diasminora2613@gmail.com>
2026-04-14 10:04:01 +0530

    add schema validator for projects and issues

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,12 +2,45 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  users: defineTable({
-    clerkId: v.string(),
-    role: v.union(v.literal("admin"), v.literal("user")),
-    name: v.optional(v.string()),
-    email: v.optional(v.string()),
-    assignedProjects: v.array(v.string()), // Linear project IDs
-  }).index("by_clerkId", ["clerkId"]),
+  developer: defineTable({
+    clerkId : v.string(), 
+    name : v.string() , 
+    email: v.string() , 
+    role : v.union( v.literal("admin") , v.literal("developer")),
+    assignedProjects : v.optional(v.array(v.string())),
+    avatarUrl : v.optional(v.string()),
+    createdAt: v.optional(v.number()), 
+  }).index("by_clerk_id" , ['clerkId']),
+
+  projects : defineTable({ // linear projects  Table 
+    linearId : v.string() , 
+    name : v.string() , 
+    description :v.string(),
+    state: v.object({
+      id : v.string(), 
+      name : v.string() ,
+      type : v.string(), 
+    }),
+    createdAt : v.number() ,
+    updatedAt: v.number(),  
+  }).index("by_linear_id" , ['linearId']),
+
+  issues : defineTable ({ // <-- linear Taskks table 
+    linearId : v.string() , 
+    title : v.string() , 
+    identifier : v.string() , 
+    description : v.string() ,
+    priority : v.string() ,
+    state : v.object({
+      name : v.string() , 
+      type  : v.optional(v.string()),
+    }),
+    assignee: v.optional(v.object({
+      name: v.string(),
+      email: v.string() , 
+    })),
+    createdAt: v.number(),
+    updatedAt: v.number(),
+  }).index("by_linear_id", ["linearId"])
   
 });
